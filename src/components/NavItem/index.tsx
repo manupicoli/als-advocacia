0f8c4744bd4f11d75ecf0a68209ce588688b8ed4
manupicoli <manuelaantunes004@gmail.com>
2025-06-29 16:44:01 -0300

    using tailwind

--- src/components/NavItem/index.tsx
+++ src/components/NavItem/index.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import "./index.css"
 
 export interface NavItemInterface {
     url: string;
@@ -9,8 +8,11 @@ export interface NavItemInterface {
 
 export default function NavItem(props: NavItemInterface) {
     return (
-         <li className="nav-item">
-            <Link href={props.url} className={`nav-link ${props.isActive ? 'active' : ''}`}>
+         <li>
+            <Link href={props.url} className={`${props.isActive 
+            ? "text-white border-[#8fcbfc]" 
+            : "text-[var(--bege)] border-transparent hover:text-white hover:border-[#8fcbfc] active:text-white active:border-[#8fcbfc] focus-visible:text-white focus-visible:border-[#8fcbfc]"}
+            text-base px-1 py-1 border-b-2 transition-colors duration-300`}>
             {props.label}
             </Link>
         </li>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import NavItem, { NavItemInterface } from "../NavItem";
 import { usePathname } from "next/navigation";
 import { FaBars, FaXmark } from "react-icons/fa6";
-import "./index.css"
 import { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 
@@ -14,6 +12,7 @@ function Navbar() {
         { url: "#hero", label: "Início"},
         { url: "#about", label: "Sobre" },
         { url: "#areas", label: "Áreas de atuação" },
+        { url: "#contact", label: "Contato"}
     ];
 
     const pathName = usePathname();
@@ -22,12 +21,16 @@ function Navbar() {
 
     return (
         <header>
-            <nav className="navbar">
-                <Link href="/" className="logo">
-                    <Image src="/logo.png" alt="Logo ALS Advocacia" width={200} height={200}/>
+            <nav className="fixed top-0 left-0 w-full bg-[var(--azul-navbar)] backdrop-blur-md text-[var(--bege)] flex justify-between items-center px-[5%] py-[10px] shadow-[0_15px_16px_-7px_rgba(0,0,0,0.1)] z-[90]">
+                <Link href="/">
+                    <img src="/logo.png" alt="Logo ALS Advocacia" width={50} height={50} className="h-full object-cover"/>
                 </Link>
                 
-                <ul className={`nav-items ${openMenu ? 'open' : ''}`}>
+                <ul className={`${openMenu ? "flex" : "hidden"}
+                    flex-col md:flex md:flex-row 
+                    gap-8 items-center list-none text-center
+                    bg-[var(--azul-navbar)] md:bg-transparent z-40
+                    absolute md:static top-[68px] md:top-auto left-0 md:left-auto w-full md:w-auto p-4 md:p-0`}>
                     {items.map((item, index) => (
                         <NavItem 
                             key={index} 
@@ -37,14 +40,14 @@ function Navbar() {
                     ))}
                 </ul>
 
-                <button className="btn-default flex items-center gap-2">
-                    <FaWhatsapp />
+                <button className="hidden md:flex rounded-[24px] px-[22px] py-[10px] cursor-pointer bg-green-600 border-none text-white flex items-center gap-2 text-base hover:bg-green-700 transition-all duration-300">
+                    <FaWhatsapp size={20}/>
                     Entre em contato
                 </button>
 
-
-                <button className="btn-mobile" onClick={() => setOpenMenu(!openMenu)}>
-                    {openMenu ? <FaXmark/> : <FaBars/>}
+                <button className="block md:hidden bg-transparent text-xl cursor-pointer border-none text-white" 
+                    onClick={() => setOpenMenu(!openMenu)}>
+                        {openMenu ? <FaXmark/> : <FaBars/>}
                 </button>
             </nav>
       </header>
