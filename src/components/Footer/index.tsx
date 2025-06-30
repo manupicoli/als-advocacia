9e349aeb588cc7b28fc9edfb3d8cca72babaae5c
manupicoli <manuelaantunes004@gmail.com>
2025-06-29 23:35:56 -0300

    fixing footer responsiveness

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,29 @@
+"use client"
+
 import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { FaPhoneSquareAlt } from "react-icons/fa";
 import { FaCopyright } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 export default function Footer() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreen = () => {
+      setIsMobile(window.innerWidth < 440);
+    };
+
+    checkScreen();
+    window.addEventListener("resize", checkScreen);
+
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
+
   return (
     <footer
       id="contact"
-      className="bg-[var(--bege)] w-full px-6 py-12 md:py-20 flex flex-col items-center text-[var(--azul-petroleo)]"
-      style={{fontFamily: 'Garamond'}}
-    >
+      className="bg-[var(--bege)] w-full px-6 py-12 md:pt-12 flex flex-col items-center text-[var(--azul-petroleo)]">
       <div className="w-full max-w-xl text-center space-y-4">
         <div className="flex justify-center items-center gap-2">
           <FaPhoneSquareAlt />
@@ -29,13 +43,38 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="mt-12 text-center">
-        <div className="flex justify-center items-center gap-2">
-          <FaCopyright size={16}/>
-          <span>2025 ALS Advocacia - Todos os direitos reservados</span>
-        </div>
-        <p className="mt-2">Desenvolvido por Manuela Picoli Antunes</p>
+      <div className="mt-12 text-center text-xs space-y-3">
+        {isMobile ? (
+           <>
+            <div className="flex justify-center items-center gap-1 leading-tight">
+              <FaCopyright/>
+              <span>2025 ALS Advocacia</span>
+            </div>
+            <p>Todos os direitos reservados</p>
+          </>
+        ) : (
+          <>
+            <div className="flex justify-center items-center gap-2 leading-relaxed">
+              <FaCopyright/>
+              <span className="whitespace-nowrap">
+                2025 ALS Advocacia - Todos os direitos reservados
+              </span>
+            </div>  
+          </>
+        )}
+        <p>
+          Desenvolvido por{" "}
+          <a
+            href="https://www.linkedin.com/in/manuela-antunes10"
+            className="border-b border-current hover:border-b-2 transition-all"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Manuela Picoli Antunes
+          </a>
+        </p>
       </div>
+
     </footer>
   );
 }
