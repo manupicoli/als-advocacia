371b49156289e2ff17649e4875c8e4fe1f6aeb28
manupicoli <manuelaantunes004@gmail.com>
2025-07-03 11:26:20 -0300

    adjusting css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import "./styles.css";
+import "../styles/globals.css";
 import Footer from "@/components/Footer";
 
 export const metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <>
-      <main>
-        <Hero />
-        <About />
-        <Areas />
-      </main>
-    </>
+    <main>
+      <Hero />
+      <About />
+      <Areas />
+    </main>
   );
 }
